27b57a5a95af1f23273f94f9a143973287fa2b63
Viniguipc <viniguipc@gmail.com>
2026-04-22 00:25:37 -0300

    revisao 1 - 6

---
+++ generic code/c/Lista Encadeada/revisao3.c
@@ -0,0 +1,295 @@
+/*. Escreva um programa que insira valores uma lista. Você pode permitir a remoção do primeiro
+elemento da lista. Quando isso ocorrer, esse elemento deve ser inserido em uma pilha e removido
+da lista. Você deve permitir ao usuário imprimir a lista e a pilha a qualquer momento.*/
+
+#include <stdio.h>
+#include <conio.h>
+#include <stdlib.h>
+
+typedef struct no{
+	int x;
+	struct no* prox;
+}lista;
+
+void imprimir_lista (lista* inicio){
+	lista *pos;
+	
+	pos = inicio;
+	
+	printf("\n");
+	
+	if(inicio == NULL){
+		printf("\nLista Vazia");
+	}
+	else{
+		while (pos != NULL){
+			printf("\n%d | [%d] -> %d", pos, pos->x, pos->prox);
+			pos = pos->prox;
+		}
+	}
+}
+
+void menu (int* op){
+	do{
+		printf("\n\n\tMENU\n");
+		printf("\n----------");
+		printf("\n1 - Inserir no Inicio\n2 - Inserir no Fim\n3 - Inserir no Meio\n4 - Excluir no Inicio\n5 - Excluir no Fim\n6 - Excluir no Meio\n7 - Inserir Ordenado\n8 - Sair");
+		printf("\n----------\n");
+		*op = _getche() - '0';
+	}while (*op < 1 || *op > 8);
+}
+
+void pedir_valor(int* x, int op){
+	if(op == 1){
+		printf("\nDigite o valor: ");
+		scanf(" %d", x);
+	}
+	else{
+		printf("\nDigite o valor buscado: ");
+		scanf(" %d", x);
+	}
+}
+
+void inserir_inicio (lista** inicio){
+	int x;
+	lista *novo;
+	
+	novo = (lista*) malloc (sizeof(lista));
+	
+	if(novo == NULL){
+		printf("\nERRO");
+	}
+	else{
+		pedir_valor(&x, 1);
+		
+		novo->x = x;
+		novo->prox = *inicio;
+		*inicio = novo;
+	}
+}
+
+void inserir_fim (lista** inicio){
+	lista *pos, *novo;
+	int x;
+	
+	pos = *inicio;
+	novo = (lista*) malloc (sizeof(lista));
+	
+	if(novo == NULL){
+		printf("\nERRO");
+	}
+	else{
+		pedir_valor(&x, 1);
+		novo->x = x;
+		
+		if(*inicio == NULL){
+			novo->prox = *inicio;
+			*inicio = novo;
+		}
+		else{
+			while(pos->prox != NULL){
+				pos = pos->prox;
+			}
+			pos->prox = novo;
+			novo->prox = NULL;
+		}
+	}
+}
+
+void inserir_meio (lista** inicio){
+	lista *pos, *ant, *novo;
+	int x, y;
+	
+	pos = *inicio;
+	novo = (lista*) malloc (sizeof(lista));
+	
+	if(novo == NULL){
+		printf("\nERRO");
+	}
+	else{
+		pedir_valor(&y, 2);
+		pedir_valor(&x, 1);
+		novo->x = x;
+		
+		if(*inicio == NULL){
+			novo->prox = *inicio;
+			*inicio = novo;
+		}
+		else{
+			while(pos->x != y && pos != NULL){
+				ant = pos;
+				pos = pos->prox;
+			}
+			
+			if(pos == NULL){
+				printf("\nValor n�o encontrado");
+			}
+			else{
+				if(pos == *inicio){
+					novo->prox = *inicio;
+					*inicio = novo;	
+				}
+				else{
+					ant->prox = novo;
+					novo->prox = pos;	
+				}
+			}
+		}
+	}
+}
+
+void excluir_inicio (lista** inicio){
+	lista* pos;
+	
+	if(*inicio == NULL){
+		printf("\nLista Vazia");		
+	}
+	else{
+		pos = *inicio;
+		*inicio = pos->prox;
+		
+		free(pos);
+	}
+}
+
+void excluir_fim (lista** inicio, lista** lixo){
+	lista *pos, *aux;
+	
+	if(*inicio == NULL){
+		printf("\nLista Vazia");
+	}
+	else{
+		pos = *inicio;
+		
+		while(pos->prox != NULL){
+			aux = pos;
+			pos = pos->prox;
+		}
+
+		aux->prox = NULL;
+		
+        pos->prox = *lixo;
+        *lixo = pos;
+        
+	}
+}
+
+void excluir_meio (lista** inicio){
+	lista *pos, *aux;
+	int y;
+	
+	if(*inicio == NULL){
+		printf("\nLista Vazia");
+	}
+	else{
+		pedir_valor(&y, 2);
+		pos = *inicio;
+		
+		while(pos->x != y && pos != NULL){
+			aux = pos;
+			pos = pos->prox;
+		}
+		if(pos == NULL){
+			printf("\nValor n�o encontrado");
+		}
+		else{
+			if(pos == *inicio){
+				*inicio = pos->prox;
+		
+				free(pos);
+			}
+			else{
+				if(pos->prox == NULL){
+					aux->prox = NULL;
+					free(pos);
+				}
+				else{
+					aux->prox = pos->prox;
+					free(pos);
+				}
+			}
+		}
+	}
+}
+
+void insere_ordenado (lista** inicio){
+	lista *pos, *ant, *novo;
+	int x;
+	
+	novo = (lista*) malloc (sizeof(lista));
+	
+	if(novo == NULL){
+		printf("\nERRo");
+	}
+	else{
+		pedir_valor(&x, 1);
+		novo->x = x;
+		pos = *inicio;
+		
+		if(*inicio == NULL){
+			novo->prox = *inicio;
+			*inicio = novo;
+		}
+		else{
+			while(pos != NULL && pos->x < novo->x){
+				ant = pos;
+				pos = pos->prox;
+			}
+			if(pos == NULL){
+				ant->prox = novo;
+				novo->prox = NULL;
+			}
+			else{
+				if(pos == *inicio){
+					*inicio = novo;
+					novo->prox = pos;
+				}
+				else{
+					ant->prox = novo;
+					novo->prox = pos;
+				}
+			}
+		}
+	}
+}
+
+int main(){
+	lista *inicio, *lixo;
+	int op;
+	
+	inicio = NULL;
+    lixo = NULL;
+	
+	do{
+		system("cls");
+		
+		imprimir_lista(inicio);
+		imprimir_lista(lixo);
+		menu(&op);
+		
+		switch(op){
+			case 1:
+				inserir_inicio(&inicio);
+				break;
+			case 2:
+				inserir_fim(&inicio);
+				break;
+			case 3:
+				inserir_meio(&inicio);
+				break;
+			case 4:
+				excluir_inicio(&inicio);
+				break;
+			case 5:
+				excluir_fim(&inicio, &lixo);
+				break;
+			case 6:
+				excluir_meio(&inicio);
+				break;
+			case 7:
+				insere_ordenado(&inicio);
+				break;
+		}
+	}while (op != 8);
+	
+}
