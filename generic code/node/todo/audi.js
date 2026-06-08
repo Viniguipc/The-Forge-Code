d912d8cd0897d7eba7cfd69167ce9a9997d31ce8
Viniguipc <viniguipc@gmail.com>
2026-06-08 16:13:29 -0300

    Node: Aprendendo estrutura de arquivos export e require

--- generic code/node/todo/audi.js
+++ generic code/node/todo/audi.js
@@ -1,1 +1,7 @@
+const obterTask = require('./task');
+
 console.log("Hello, World!");
+let lista = obterTask();
+console.log(lista);
+
+
---
+++ generic code/node/todo/task.js
@@ -0,0 +1,5 @@
+function obterTask() {
+	return ["Criar Arquivo JSON", "Abrir Arquivo JSON", "Adicionar Tarefa no JSON", "Remover Tarefa no JSON"];
+}
+
+module.exports = obterTask;
