a60eeb8a3bd28223a7b8b8a81da52588c1bbb82c
Viniguipc <viniguipc@gmail.com>
2026-06-08 16:20:47 -0300

    Node: Criando servidor com a porta 3000

--- generic code/node/todo/audi.js
+++ generic code/node/todo/audi.js
@@ -1,7 +1,14 @@
+const http = require ('http');
 const obterTask = require('./task');
 
-console.log("Hello, World!");
-let lista = obterTask();
-console.log(lista);
+const servidor = http.createServer((req, res) => {
+	res.writeHead(200, {'Content-Type': 'text/plain; charset=utf-8'});
+	
+	res.end('Hello, World!');
+});
+
+servidor.listen(3000, () => {
+	console.log('localhost 3000');
+});
 
 
